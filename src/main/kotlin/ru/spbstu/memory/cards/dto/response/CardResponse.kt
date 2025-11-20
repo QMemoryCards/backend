f603c3c25cf8fc5ece49c2bdc2d9f9beb8dd3ac6
Max <lonishin.max@gmail.com>
2025-11-20 22:39:22 +0300

    tiny change was done

--- src/main/kotlin/ru/spbstu/memory/cards/dto/response/CardResponse.kt
+++ src/main/kotlin/ru/spbstu/memory/cards/dto/response/CardResponse.kt
@@ -1,16 +1,17 @@
 package ru.spbstu.memory.cards.dto.response
 
 import ru.spbstu.memory.cards.persistence.model.Card
+import java.util.UUID
 
 data class CardResponse(
-    val id: String,
+    val id: UUID,
     val question: String,
     val answer: String,
 ) {
     companion object {
         fun from(card: Card) =
             CardResponse(
-                id = card.id.toString(),
+                id = card.id,
                 question = card.question,
                 answer = card.answer,
             )
